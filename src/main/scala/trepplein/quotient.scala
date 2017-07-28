8b05c69b8a605ac3f3a2383860b18bd1b9d39923
Gabriel Ebner <gebner@gebner.org>
2017-07-28 15:54:01 +0100

    Simplify quotient extension.

--- src/main/scala/trepplein/quotient.scala
+++ src/main/scala/trepplein/quotient.scala
@@ -1,81 +1,49 @@
 package trepplein
 
-object Quot {
-  val name = Name("quot")
+object quotient {
   val univParams = Vector(Level.Param(Name("u")))
   val A = LocalConst(Binding(Name("A"), Sort(univParams(0)), BinderInfo.Implicit))
   val R = LocalConst(Binding(Name("R"), A -->: A -->: Sort.Prop, BinderInfo.Default))
-  val ty = Pis(A, R)(Sort(univParams(0)))
-  val decl = Axiom(name, univParams, ty, builtin = true)
-}
-object QuotMk {
-  val name = Name.Str(Quot.name, "mk")
-  val univParams = Quot.univParams
 
-  val A = Quot.A
-  val R = Quot.R
-  val ty = Pis(A, R)(A -->: Apps(Const(Quot.name, univParams), A, R))
-  val decl = Axiom(name, univParams, ty, builtin = true)
-}
-object QuotLift {
-  val name = Name.Str(Quot.name, "lift")
-  val univParams = Quot.univParams :+ Level.Param(Name("v"))
+  val quot = Axiom(Name("quot"), univParams, Pis(A, R)(Sort(univParams(0))), builtin = true)
 
-  val A = Quot.A
-  val R: LocalConst = LocalConst(Quot.R.of.copy(info = BinderInfo.Implicit))
-  val B = LocalConst(Binding(Name("B"), Sort(univParams(1)), BinderInfo.Implicit))
+  val quotMk = Axiom(Name.Str(quot.name, "mk"), univParams,
+    Pis(A, R)(A -->: Apps(Const(quot.name, univParams), A, R)), builtin = true)
+
+  val liftUnivParams = univParams :+ Level.Param(Name("v"))
+  val B = LocalConst(Binding(Name("B"), Sort(liftUnivParams(1)), BinderInfo.Implicit))
   val f = LocalConst(Binding(Name("f"), A -->: B, BinderInfo.Default))
   val Seq(a, b) = Seq("a", "b").map(n => LocalConst(Binding(Name(n), A, BinderInfo.Default)))
-  val ty = Pis(A, R, B, f)(
-    Pis(a, b)(Apps(R, a, b) -->: Apps(Const(Name("eq"), Vector(univParams(1))), B, App(f, a), App(f, b))) -->:
-      Apps(Const(Quot.name, Vector(univParams(0))), A, R) -->: B
-  )
-  val decl = Axiom(name, univParams, ty, builtin = true)
-}
-object QuotInd {
-  val name = Name.Str(Quot.name, "ind")
-  val univParams = Quot.univParams
 
-  val A = Quot.A
-  val R: LocalConst = LocalConst(Quot.R.of.copy(info = BinderInfo.Implicit))
-  val B = LocalConst(Binding(Name("B"), Apps(Const(Quot.name, Vector(univParams(0))), A, R) -->: Sort.Prop, BinderInfo.Implicit))
-  val a = LocalConst(Binding(Name("a"), A, BinderInfo.Default))
-  val q = LocalConst(Binding(
-    Name("q"),
-    Apps(Const(Quot.name, Vector(univParams(0))), A, R), BinderInfo.Default
-  ))
-  val ty = Pis(A, R, B)(
-    Pi(a, Apps(B, Apps(Const(QuotMk.name, Vector(univParams(0))), A, R, a))) -->:
-      Pi(q, Apps(B, q))
+  val quotLift = Axiom(Name.Str(quot.name, "lift"), liftUnivParams,
+    Pis(A, R, B, f)(
+      Pis(a, b)(Apps(R, a, b) -->: Apps(Const(Name("eq"), Vector(liftUnivParams(1))), B, App(f, a), App(f, b))) -->:
+        Apps(Const(quot.name, Vector(liftUnivParams(0))), A, R) -->: B
+    ),
+    builtin = true)
+
+  val B2 = LocalConst(Binding(Name("B"), Apps(Const(quot.name, Vector(univParams(0))), A, R) -->: Sort.Prop, BinderInfo.Implicit))
+  val q = LocalConst(Binding(Name("q"), Apps(Const(quot.name, Vector(univParams(0))), A, R), BinderInfo.Default))
+  val quotInd = Axiom(Name.Str(quot.name, "ind"), univParams, Pis(A, R, B2)(
+    Pi(a, Apps(B2, Apps(Const(quotMk.name, Vector(univParams(0))), A, R, a))) -->: Pi(q, Apps(B2, q))
+  ), builtin = true)
+
+  val h = LocalConst(Binding(Name("h"), Apps(Const(Name("eq"), Vector(liftUnivParams(1))), B, App(f, a), App(f, b)), BinderInfo.Default))
+  val quotRed = ReductionRule(
+    Vector(A, R, B, f, a, h),
+    Apps(Const(quotLift.name, univParams), A, R, B, f, h, Apps(Const(quotMk.name, Vector(univParams(0))), A, R, a)),
+    Apps(f, a),
+    List()
   )
-  val decl = Axiom(name, univParams, ty, builtin = true)
 }
 
 case object QuotMod extends Modification {
-  def name: Name = Quot.name
+  import quotient._
+  def name: Name = quot.name
   def compile(env: PreEnvironment) = new CompiledModification {
-    val decls = Seq(Quot.decl, QuotMk.decl, QuotInd.decl, QuotLift.decl)
-
-    override def check(): Unit = {
-      // TODO: propext, eq
+    val decls = Seq(quot, quotMk, quotInd, quotLift)
+    val rules = Seq(quotRed)
+    override def check(): Unit =
       decls.foldLeft(env)((env, decl) => env.addNow(decl.asAxiom))
-    }
-
-    val rules: Seq[ReductionRule] = {
-      import QuotLift._
-      val h = LocalConst(Binding(
-        Name("h"),
-        Apps(Const(Name("eq"), Vector(univParams(1))), B, App(f, a), App(f, b)), BinderInfo.Default
-      ))
-      Seq(
-        ReductionRule(
-          Vector(A, R, B, f, a, h),
-          Apps(Const(QuotLift.name, univParams), A, R, B, f, h,
-            Apps(Const(QuotMk.name, Vector(univParams(0))), A, R, a)),
-          Apps(f, a),
-          List()
-        )
-      )
-    }
   }
 }
